5422f2739b3a95ed6364b44bd8ee4d2125d624ea
CarrotRub <riadbouadjenek1@gmail.com>
2024-08-30 23:18:29 +0200

    FEATURES AND BUG FIXES : FIT LAUNCHER VERSION 1.0.1
    Fixed issue with downloading not starting sometimes due to oneshot channel concurrency issue.
    Added feature to add already downloaded games.
    Added feature to remove downloaded games by right-clicking on it and chosing remove game.
    Normalized the path.
    Fixed issue with directx and c++ checkboxes.

--- src/components/Gamedownloadvertical-01/Gamedownloadvertical.jsx
+++ src/components/Gamedownloadvertical-01/Gamedownloadvertical.jsx
@@ -1,10 +1,9 @@
 import { createSignal, onCleanup, onMount } from "solid-js";
-
 import Chart from "chart.js/auto";
 import { invoke } from "@tauri-apps/api";
 import './Gamedownloadvertical.css';
 
-function Gameverticaldownloadslide({ isActive }) {
+function Gameverticaldownloadslide({ isActive, setIsActive }) {
     const [gameInfo, setGameInfo] = createSignal(null);
     const [loading, setLoading] = createSignal(true);
     const [percentage, setPercentage] = createSignal(0);
@@ -17,29 +16,30 @@ function Gameverticaldownloadslide({ isActive }) {
             const stats = JSON.parse(localStorage.getItem('CDG_Stats'));
             
             setGameInfo(stats);
-            setPercentage((gameInfo()?.progress_bytes / gameInfo()?.total_bytes * 100).toFixed(2));
+            const progressPercentage = (gameInfo()?.progress_bytes / gameInfo()?.total_bytes * 100).toFixed(2);
+            setPercentage(isNaN(progressPercentage) ? 0 : progressPercentage);
             setLoading(false);
             updateCharts(stats);
+
         } catch (error) {
             console.error('Error fetching torrent stats:', error);
         }
     };
-// Function to handle pause/resume button click
+
     const handleButtonClick = async () => {
-        
         const currentState = gameInfo().state;
         try {
             if (currentState === 'paused') {
                 await invoke('resume_torrent_command');
-                setIsPaused(false); // The state was paused, now it is resumed
+                setIsPaused(false);
             } else if (currentState === 'live') {
                 await invoke('pause_torrent_command');
-                setIsPaused(true); // The state was live, now it is paused
+                setIsPaused(true);
             }
         } catch (error) {
             console.error('Error toggling torrent state:', error);
         }
-      };
+    };
 
     const PauseResumeSvg = () => {
         const iconCorr = () => {
@@ -47,66 +47,65 @@ function Gameverticaldownloadslide({ isActive }) {
 
             switch (state) {
                 case 'paused':
-                  return (
-                    <svg width="16" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M8 5l10 7-10 7V5z" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                  );
+                    return (
+                        <svg width="16" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M8 5l10 7-10 7V5z" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    );
                 case 'live':
-                  return (
-                    <svg width="16" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M8 5v14m8-14v14" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                  );
+                    return (
+                        <svg width="16" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M8 5v14m8-14v14" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    );
                 default:
                     return (
                         <svg width="16" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M8 5v14m8-14v14" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
-                      );
-              }
+                    );
+            }
         }
 
         return (
             <span class="icon" onClick={handleButtonClick}>{iconCorr}</span> 
-        )
-}
+        );
+    };
 
-      const PauseResumeButton = () => {
+    const PauseResumeButton = () => {
         const buttonText = () => {
-          try {
-            const state = gameInfo()?.state;
-            if (state === null || state === undefined) {
-              return 'Inactive';
+            try {
+                const state = gameInfo()?.state;
+                if (state === null || state === undefined) {
+                    return 'Inactive';
+                }
+                switch (state) {
+                    case 'paused':
+                        return 'Resume';
+                    case 'live':
+                        return 'Pause';
+                    case 'initializing':
+                        return 'Loading...';
+                    default:
+                        return 'Unknown State';
+                }
+            } catch (error) {
+                console.error('Error determining button text:', error);
+                return 'Error';
             }
-            switch (state) {
-              case 'paused':
-                return 'Resume';
-              case 'live':
-                return 'Pause';
-              case 'initializing':
-                return 'Loading...';
-              default:
-                return 'Unknown State';
-            }
-          } catch (error) {
-            console.error('Error determining button text:', error);
-            return 'Error';
-          }
         };
-      
+
         return (
             <button onClick={handleButtonClick}>
-              {buttonText()}
+                {buttonText()}
             </button>
-          );
-      };
-
+        );
+    };
 
     const updateCharts = (stats) => {
         if (downloadUploadChart && bytesChart) {
-            const mbDownloadSpeed = (stats.download_speed || 0) ;
-            const mbUploadSpeed = (stats.upload_speed || 0) ;
+            const mbDownloadSpeed = (stats.download_speed || 0);
+            const mbUploadSpeed = (stats.upload_speed || 0);
             const downloadedMB = (stats.progress_bytes || 0) / (1024 * 1024);
             const uploadedMB = (stats.uploaded_bytes || 0) / (1024 * 1024);
 
@@ -119,16 +118,18 @@ function Gameverticaldownloadslide({ isActive }) {
             downloadUploadChart.data.labels.push("");
             bytesChart.data.labels.push("");
 
-
             downloadUploadChart.update();
             bytesChart.update();
         }
     };
-    
 
     onMount(() => {
         fetchStats();
         const intervalId = setInterval(fetchStats, 500);
+        
+        if (gameInfo().finished) {
+            clearInterval(intervalId);
+        }
         onCleanup(() => clearInterval(intervalId));
 
         // Initialize charts
@@ -145,7 +146,6 @@ function Gameverticaldownloadslide({ isActive }) {
                         backgroundColor: 'rgba(75, 192, 192, 0.2)',
                         fill: false,
                         pointStyle: false
-                        
                     },
                     {
                         label: 'Upload Speed (MB/s)',
@@ -217,32 +217,46 @@ function Gameverticaldownloadslide({ isActive }) {
             }
         });
     });
-    
+
     const handleStopTorrent = () => {
         invoke('stop_torrent_command');
         localStorage.removeItem('CDG');
         window.dispatchEvent(new Event('storage'));
-        console.log("donea")
+    }
+
+    const slideLeft = () => {
+        localStorage.setItem('isSidebarActive', false);
+        setIsActive(false);
+    }
+
+    // Ensure that the percentage is valid before rendering the component
+    if (isNaN(percentage()) || percentage() === null) {
+        return null;
     }
 
     return (
-        
         <div class="sidebar-space" style={{ display: isActive ? 'block' : 'none' }}>
-            
+            <div class="arrow-container left" onClick={slideLeft}>
+                <div class="arrow-down"></div>
+            </div>
             <div class="stats-panel">
                 <h2>Game Download Progress</h2>
                 <div class="progress-container">
                     <div class="progress-bar">
-
                         <div class="progress" style={{ width: `${percentage()}%` }}></div>
                         <span class="progress-text">DOWNLOADING {percentage()}%</span>
                         <div class="icons">
-                        <span class="icon" onClick={handleStopTorrent}><svg width="16" height="24" viewBox="-0.5 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg"><g stroke-width="0"/><g stroke-linecap="round" stroke-linejoin="round"/><path d="m3 21.32 18-18m-18 0 18 18" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg></span>
-                        <div class="icon-divider"></div>
-                        <PauseResumeSvg/>
+                            <span class="icon" onClick={handleStopTorrent}>
+                                <svg width="16" height="24" viewBox="-0.5 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g stroke-width="0"/>
+                                    <g stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path d="m3 21.32 18-18m-18 0 18 18" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                            </span>
+                            <div class="icon-divider"></div>
+                            <PauseResumeSvg/>
+                        </div>
                     </div>
-                    </div>
-
                 </div>
                 <canvas id="downloadUploadChart"></canvas>
                 <canvas id="bytesChart"></canvas>
