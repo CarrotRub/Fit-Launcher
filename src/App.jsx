cd720a75046fbab324e5a8759c43372f87beb4cd
carlossclaro <stayputman@gmail.com>
2024-10-05 13:23:21 -0400

    Implementation and Enhancements
    - Changelog Feature: Added a changelog popup to display new features, bug fixes, and improvements.
    - Network Error Handling: Implemented alerts to notify users of network failures during startup.
    - Enhanced Logging: Increased logging during scraping and network tasks for better debugging and traceability.
    - UI Optimization Prep: Prepared the codebase for updating individual UI components without requiring a full window reload.
    - Background Image Preparation: Added groundwork to allow for changing background images in future updates.
    - Manual Refresh Feature: Started implementation for a manual refresh feature to check for game content updates.
    - Code Improvements: Various minor code optimizations to enhance maintainability and performance.

--- src/App.jsx
+++ src/App.jsx
@@ -1,158 +1,189 @@
-import { createEffect, onMount } from 'solid-js';
-import { invoke } from '@tauri-apps/api';
-import { appWindow } from '@tauri-apps/api/window';
-import { listen } from '@tauri-apps/api/event';
-import { Router } from '@solidjs/router';
-import { lazy } from 'solid-js'; // Import lazy here
+import { onMount, createSignal } from 'solid-js'
+import { appWindow } from '@tauri-apps/api/window'
+import { listen } from '@tauri-apps/api/event'
+import { Router } from '@solidjs/router'
+import { lazy } from 'solid-js'
+import { emit } from '@tauri-apps/api/event'
 
-import Sidebar from './components/Sidebar-01/Sidebar';
-import Searchbar from './components/Searchbar-01/Searchbar';
+import Sidebar from './components/Sidebar-01/Sidebar'
+import Searchbar from './components/Searchbar-01/Searchbar'
+import Notification from './components/Notification-01/Notification'
+import ChangelogPopup from './components/Changelog-01/ChangelogPopup'
 
-import './App.css';
-import './templates/titlebar-01/titlebar.css';
+import './App.css'
+import './templates/titlebar-01/titlebar.css'
 
 function App() {
-  onMount(() => {
+    const [isDialogOpen, setIsDialogOpen] = createSignal(false)
+    const [notificationMessage, setNotificationMessage] = createSignal('')
 
-    // //2024-09-05 - Placeholder code for a later update to handle network alert errors
-    // // Listen for the scraping_failed event globally
-    // listen('scraping_failed', (event) => {
-    //   console.log("scrap_fail_alert");
-    //   //alert(event.payload); // Display the alert when scraping fails
-    // });
+    onMount(() => {
+        console.log('App mounted. Setting up event listeners...')
+        // Emit an event to the backend indicating that the frontend is ready
+        emit('frontend-ready')
 
-    // Event listeners for window controls
-    document
-      .getElementById('titlebar-minimize')
-      .addEventListener('click', () => appWindow.minimize());
-    document
-      .getElementById('titlebar-maximize')
-      .addEventListener('click', () => appWindow.toggleMaximize());
-    document
-      .getElementById('titlebar-close')
-      .addEventListener('click', () => {
-        let cdgStats = localStorage.getItem('CDG_Stats');
-        console.log('Current CDG_Stats from localStorage:', cdgStats);
+        // Listen for the network failure event
+        listen('network-failure', (event) => {
+            setNotificationMessage(`Network failure: ${event.payload.message}`)
+            setIsDialogOpen(true)
+        })
 
-        try {
-          cdgStats = JSON.parse(cdgStats);
-          console.log('Parsed CDG_Stats:', cdgStats);
+        // Event listeners for window controls
+        document
+            .getElementById('titlebar-minimize')
+            .addEventListener('click', () => appWindow.minimize())
+        document
+            .getElementById('titlebar-maximize')
+            .addEventListener('click', () => appWindow.toggleMaximize())
+        document
+            .getElementById('titlebar-close')
+            .addEventListener('click', () => {
+                let cdgStats = localStorage.getItem('CDG_Stats')
+                console.log('Current CDG_Stats from localStorage:', cdgStats)
 
-          if (cdgStats) {
-            cdgStats.state = 'paused';
-            console.log('Updated CDG_Stats:', cdgStats);
+                try {
+                    cdgStats = JSON.parse(cdgStats)
+                    console.log('Parsed CDG_Stats:', cdgStats)
 
-            localStorage.setItem('CDG_Stats', JSON.stringify(cdgStats));
+                    if (cdgStats) {
+                        cdgStats.state = 'paused'
+                        console.log('Updated CDG_Stats:', cdgStats)
 
-            const updatedCdgStats = localStorage.getItem('CDG_Stats');
-            console.log(
-              'Updated CDG_Stats saved in localStorage:',
-              updatedCdgStats
-            );
-          } else {
-            console.error(
-              "CDG_Stats is not in the expected format or missing 'state' property:",
-              cdgStats
-            );
-          }
-        } catch (error) {
-          console.error('Error parsing CDG_Stats:', error);
-        }
-        appWindow.close();
-      });
-  });
+                        localStorage.setItem(
+                            'CDG_Stats',
+                            JSON.stringify(cdgStats)
+                        )
 
-  const sidebarRoutes = [
-    {
-      path: "/",
-      component: lazy(() => import("./templates/Gamehub-01/Gamehub")),
-    },
-    {
-      path: "/my-library",
-      component: lazy(() => import("./templates/mylibrary-01/Mylibrary")),
-    },
-    {
-      path: "/settings",
-      component: lazy(() => import("./templates/Settings-01/Settings")),
-    },
-  ];
+                        const updatedCdgStats =
+                            localStorage.getItem('CDG_Stats')
+                        console.log(
+                            'Updated CDG_Stats saved in localStorage:',
+                            updatedCdgStats
+                        )
+                    } else {
+                        console.error(
+                            "CDG_Stats is not in the expected format or missing 'state' property:",
+                            cdgStats
+                        )
+                    }
+                } catch (error) {
+                    console.error('Error parsing CDG_Stats:', error)
+                }
+                appWindow.close()
+            })
+    })
 
-  return (
-    <>
-      <div>
-        <div>
-          <div data-tauri-drag-region class="titlebar">
-            <div class="titlebar-button" id="titlebar-minimize">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="1em"
-                height="1em"
-                viewBox="0 0 16 16"
-              >
-                <path fill="currentColor" d="M14 8v1H3V8z"></path>
-              </svg>
-            </div>
-            <div class="titlebar-button" id="titlebar-maximize">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="1em"
-                height="1em"
-                viewBox="0 0 16 16"
-              >
-                <g fill="currentColor">
-                  <path d="M3 5v9h9V5zm8 8H4V6h7z"></path>
-                  <path
-                    fillRule="evenodd"
-                    d="M5 5h1V4h7v7h-1v1h2V3H5z"
-                    clipRule="evenodd"
-                  ></path>
-                </g>
-              </svg>
-            </div>
-            <div class="titlebar-button" id="titlebar-close">
-              <svg
-                className="icon-id"
-                xmlns="http://www.w3.org/2000/svg"
-                width="1em"
-                height="1em"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fill="currentColor"
-                  fillRule="evenodd"
-                  d="m7.116 8-4.558 4.558.884.884L8 8.884l4.558 4.558.884-.884L8.884 8l4.558-4.558-.884-.884L8 7.116 3.442 2.558l-.884.884z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </div>
-          </div>
-        </div>
+    function closeDialog() {
+        setIsDialogOpen(false)
+    }
 
-        {/* Contains the sidebar. DO NOT REMOVE. */}
-        <Router
-          root={(props) => (
-            <>
-              <div>
-                <div className="sidebar">
-                  <Sidebar />
-                </div>
-                <div className="app-container">
-                  <div className="main-content">
-                    <div className="searchbar">
-                      <Searchbar />
+    const sidebarRoutes = [
+        {
+            path: '/',
+            component: lazy(() => import('./templates/Gamehub-01/Gamehub')),
+        },
+        {
+            path: '/my-library',
+            component: lazy(() => import('./templates/mylibrary-01/Mylibrary')),
+        },
+        {
+            path: '/settings',
+            component: lazy(() => import('./templates/Settings-01/Settings')),
+        },
+    ]
+
+    return (
+        <>
+            <div>
+                <div>
+                    <div data-tauri-drag-region class="titlebar">
+                        <div class="titlebar-button" id="titlebar-minimize">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="1em"
+                                height="1em"
+                                viewBox="0 0 16 16"
+                            >
+                                <path
+                                    fill="currentColor"
+                                    d="M14 8v1H3V8z"
+                                ></path>
+                            </svg>
+                        </div>
+                        <div class="titlebar-button" id="titlebar-maximize">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="1em"
+                                height="1em"
+                                viewBox="0 0 16 16"
+                            >
+                                <g fill="currentColor">
+                                    <path d="M3 5v9h9V5zm8 8H4V6h7z"></path>
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M5 5h1V4h7v7h-1v1h2V3H5z"
+                                        clipRule="evenodd"
+                                    ></path>
+                                </g>
+                            </svg>
+                        </div>
+                        <div class="titlebar-button" id="titlebar-close">
+                            <svg
+                                className="icon-id"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="1em"
+                                height="1em"
+                                viewBox="0 0 16 16"
+                            >
+                                <path
+                                    fill="currentColor"
+                                    fillRule="evenodd"
+                                    d="m7.116 8-4.558 4.558.884.884L8 8.884l4.558 4.558.884-.884L8.884 8l4.558-4.558-.884-.884L8 7.116 3.442 2.558l-.884.884z"
+                                    clipRule="evenodd"
+                                ></path>
+                            </svg>
+                        </div>
                     </div>
-                    {props.children}
-                  </div>
                 </div>
-              </div>
-            </>
-          )}
-        >
-          {sidebarRoutes}
-        </Router>
-      </div>
-    </>
-  );
+
+                <Router
+                    root={(props) => (
+                        <>
+                            <div>
+                                <div className="sidebar">
+                                    <Sidebar />
+                                </div>
+                                <div className="app-container">
+                                    <ChangelogPopup />
+                                    <div className="main-content">
+                                        <div className="searchbar">
+                                            <Searchbar />
+                                        </div>
+                                        <div
+                                            className={`notification-wrapper ${
+                                                !isDialogOpen() ? 'hidden' : ''
+                                            }`}
+                                        >
+                                            {isDialogOpen() && (
+                                                <Notification
+                                                    message={notificationMessage()}
+                                                    type="error"
+                                                    onClose={closeDialog}
+                                                />
+                                            )}
+                                        </div>
+                                        {props.children}
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )}
+                >
+                    {sidebarRoutes}
+                </Router>
+            </div>
+        </>
+    )
 }
 
-export default App;
+export default App
