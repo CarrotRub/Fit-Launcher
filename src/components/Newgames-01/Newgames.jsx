b8b46dd326b5a369c6503971a95f110decda9de8
carlossclaro <stayputman@gmail.com>
2024-10-18 05:26:34 +0300

    v1.0.3 - Code Improvements
    - Peer information is now displayed during torrent downloads.
    - Sidebar scrollbar is hidden for a cleaner look.
    - Replaced "FILTER" text with a filter icon.
    - Improved icon alignment in the download progress bar.

--- src/components/Newgames-01/Newgames.jsx
+++ src/components/Newgames-01/Newgames.jsx
@@ -1,61 +1,61 @@
-import './Newgames.css';
-import { createEffect, createSignal, onMount } from 'solid-js';
-import { appDataDir } from '@tauri-apps/api/path';
-import readFile from '../functions/readFileRust';
-import Slider from '../Slider-01/Slider';
-import Swal from 'sweetalert2';
+import './Newgames.css'
+import { createEffect, createSignal, onMount } from 'solid-js'
+import { appDataDir } from '@tauri-apps/api/path'
+import readFile from '../functions/readFileRust'
+import Slider from '../Slider-01/Slider'
+import Swal from 'sweetalert2'
 
-const appDir = await appDataDir();
-const dirPath = appDir;
-const newlyAddedGamesPath = `${dirPath}tempGames/newly_added_games.json`;
+const appDir = await appDataDir()
+const dirPath = appDir
+const newlyAddedGamesPath = `${dirPath}tempGames/newly_added_games.json`
 
 /**
  * Get newly added games into the GameHub.
  */
 async function parseNewGameData() {
     try {
-        const fileContent = await readFile(newlyAddedGamesPath);
-        const gameData = JSON.parse(fileContent.content);
+        const fileContent = await readFile(newlyAddedGamesPath)
+        const gameData = JSON.parse(fileContent.content)
 
         // Load the user's settings to check if NSFW content should be hidden
-        const settingsPath = `${dirPath}/fitgirlConfig/settings.json`;
-        const settingsContent = await readFile(settingsPath);
-        const settings = JSON.parse(settingsContent.content);
-        const hideNSFW = settings.hide_nsfw_content;
+        const settingsPath = `${dirPath}/fitgirlConfig/settings.json`
+        const settingsContent = await readFile(settingsPath)
+        const settings = JSON.parse(settingsContent.content)
+        const hideNSFW = settings.hide_nsfw_content
 
         // Filter out NSFW games based on the "Adult" tag if the setting is enabled
         const filteredGameData = hideNSFW
-            ? gameData.filter(game => !game.tag.includes('Adult'))
-            : gameData;
+            ? gameData.filter((game) => !game.tag.includes('Adult'))
+            : gameData
 
-        return filteredGameData;
+        return filteredGameData
     } catch (error) {
-        console.error('Error parsing game data:', error);
-        throw error;
+        console.error('Error parsing game data:', error)
+        throw error
     }
 }
 
 function Newgames() {
-    const [imagesObject, setImagesObject] = createSignal([]);
-    const [tags, setTags] = createSignal([]); // All unique tags
-    const [selectedTags, setSelectedTags] = createSignal([]); // Selected tags
-    const [filteredImages, setFilteredImages] = createSignal([]); // Images after filtering
-    const [sliderComponent, setSliderComponent] = createSignal(null); // Hold slider component
+    const [imagesObject, setImagesObject] = createSignal([])
+    const [tags, setTags] = createSignal([]) // All unique tags
+    const [selectedTags, setSelectedTags] = createSignal([]) // Selected tags
+    const [filteredImages, setFilteredImages] = createSignal([]) // Images after filtering
+    const [sliderComponent, setSliderComponent] = createSignal(null) // Hold slider component
 
     onMount(async () => {
         try {
-            const data = await parseNewGameData();
-            setImagesObject(data);
+            const data = await parseNewGameData()
+            setImagesObject(data)
 
-            const allTags = new Set();
-            data.forEach(game => {
-                const tagsArray = game.tag.split(',').map(tag => tag.trim());
-                tagsArray.forEach(tag => allTags.add(tag));
-            });
-            setTags(Array.from(allTags));
+            const allTags = new Set()
+            data.forEach((game) => {
+                const tagsArray = game.tag.split(',').map((tag) => tag.trim())
+                tagsArray.forEach((tag) => allTags.add(tag))
+            })
+            setTags(Array.from(allTags))
 
             // Initialize filtered images to show all initially
-            setFilteredImages(data);
+            setFilteredImages(data)
         } catch (error) {
             Swal.fire({
                 title: 'Error',
@@ -72,25 +72,26 @@ function Newgames() {
                 `,
                 footer: `Error: ${error}`,
                 icon: 'error',
-                confirmButtonText: 'Ok'
-            });
+                confirmButtonText: 'Ok',
+            })
         }
-    });
+    })
 
     // Effect to filter images whenever selectedTags change
     createEffect(() => {
-        const currentImages = imagesObject();
-        const currentSelectedTags = selectedTags();
+        const currentImages = imagesObject()
+        const currentSelectedTags = selectedTags()
 
         // If no tags are selected, set filtered images to all images
         if (currentSelectedTags.length === 0) {
-            setFilteredImages(currentImages);
+            setFilteredImages(currentImages)
         } else {
             // Filter images based on selected tags
-            const newFilteredImages = currentImages.filter(game =>
-                currentSelectedTags.every(tag => game.tag.includes(tag)) // Change here to use 'every'
-            );
-            setFilteredImages(newFilteredImages);
+            const newFilteredImages = currentImages.filter(
+                (game) =>
+                    currentSelectedTags.every((tag) => game.tag.includes(tag)) // Change here to use 'every'
+            )
+            setFilteredImages(newFilteredImages)
         }
 
         // Render Slider when filteredImages changes
@@ -104,47 +105,74 @@ function Newgames() {
                     showPrevNextButtons={true}
                 />
             ) : null
-        );
-    });
+        )
+    })
 
     const toggleTagSelection = (tag) => {
         setSelectedTags((prevTags) => {
             // If tag is already selected, remove it
             if (prevTags.includes(tag)) {
-                return prevTags.filter(t => t !== tag);
+                return prevTags.filter((t) => t !== tag)
             }
             // If tag is not selected, add it
-            return [...prevTags, tag];
-        });
-    };
+            return [...prevTags, tag]
+        })
+    }
 
     const resetFilters = () => {
-        setSelectedTags([]); 
-    };
+        setSelectedTags([])
+    }
 
     return (
         <>
-            <div className='title-category newgames'>
+            <div className="title-category newgames">
                 <h2>Newly Added Games</h2>
-                <div className='filter-box'>
-                    <details className='filter-details newgames'>
-                        <summary onClick={(e) => {
-                            e.preventDefault();
-                            const details = document.querySelector(".filter-details.newgames");
-                            details.open = !details.open; // Toggle the open state
-                        }}>
-                            Filter {selectedTags().length > 0 && <span>({selectedTags().length})</span>}
+                <div className="filter-box">
+                    <details className="filter-details newgames">
+                        <summary
+                            onClick={(e) => {
+                                e.preventDefault()
+                                const details = document.querySelector(
+                                    '.filter-details.newgames'
+                                )
+                                details.open = !details.open // Toggle the open state
+                            }}
+                        >
+                            <svg
+                                className="filter-icon"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                viewBox="0 0 24 24"
+                            >
+                                <polygon points="22 3 2 3 10 13 10 19 14 21 14 13 22 3"></polygon>
+                            </svg>
+                            {selectedTags().length > 0 && (
+                                <span>({selectedTags().length})</span>
+                            )}
                         </summary>
-                        <ul className='tags-list'>
-                            {tags().map(tag => (
-                                <li key={tag} onClick={() => toggleTagSelection(tag)}>
-                                    <div className='checkbox-wrapper'>
+                        <ul className="tags-list">
+                            {tags().map((tag) => (
+                                <li
+                                    key={tag}
+                                    onClick={() => toggleTagSelection(tag)}
+                                >
+                                    <div className="checkbox-wrapper">
                                         <label>
-                                            <input 
-                                                type="checkbox" 
-                                                checked={selectedTags().includes(tag)} 
-                                                onChange={() => toggleTagSelection(tag)} 
-                                                className='custom-checkbox'
+                                            <input
+                                                type="checkbox"
+                                                checked={selectedTags().includes(
+                                                    tag
+                                                )}
+                                                onChange={() =>
+                                                    toggleTagSelection(tag)
+                                                }
+                                                className="custom-checkbox"
                                             />
                                             {tag}
                                         </label>
@@ -162,7 +190,7 @@ function Newgames() {
             </div>
             {sliderComponent()} {/* Render Slider component here */}
         </>
-    );
+    )
 }
 
-export default Newgames;
+export default Newgames
