1c780182b07446173e13f1809f285339e4ea52d6
CarrotRub <riadbouadjenek1@gmail.com>
2024-12-23 16:29:15 +0100

    Final 2.0.0 commit version :
    Added 2.0.0 version here with every changes

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,6 +1,7 @@
 fn main() {
     let mut windows = tauri_build::WindowsAttributes::new();
-    windows = windows.app_manifest(r#"
+    windows = windows.app_manifest(
+        r#"
     <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
       <dependency>
         <dependentAssembly>
@@ -22,8 +23,8 @@ fn main() {
         </security>
       </trustInfo>
     </assembly>
-    "#);
-    tauri_build::try_build(
-      tauri_build::Attributes::new().windows_attributes(windows)
-    ).expect("failed to run build script");
+    "#,
+    );
+    tauri_build::try_build(tauri_build::Attributes::new().windows_attributes(windows))
+        .expect("failed to run build script");
 }
