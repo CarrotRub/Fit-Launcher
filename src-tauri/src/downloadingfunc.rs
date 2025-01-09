6d1f915d7413a108214a957ab775ac26efd5d56a
CarrotRub <riadbouadjenek1@gmail.com>
2025-01-09 14:58:05 +0100

    Added full fetchinf of datahoster links support

--- src-tauri/src/downloadingfunc.rs
+++ src-tauri/src/downloadingfunc.rs
@@ -1,8 +1,8 @@
 pub mod downloads_function {
     use anyhow::Result;
-    use tracing::{error, info};
+    use serde::{Deserialize, Serialize};
 
-    use crate::{basic_scraping::ScrapingError, CUSTOM_DNS_CLIENT};
+    use crate::CUSTOM_DNS_CLIENT;
 
     #[derive(Debug)]
     struct GameInfoLinks {
@@ -19,69 +19,45 @@ pub mod downloads_function {
         }
     }
 
-    #[tokio::main]
-    async fn get_links_list(link_to_paste: String) -> Result<Vec<String>, Box<ScrapingError>> {
-        let response = CUSTOM_DNS_CLIENT
-            .get(&link_to_paste)
-            .send()
-            .await
-            .map_err(|e| {
-                error!("Failed to get response from URL: {}", &link_to_paste);
-                error!("Error is: {}", e);
-                ScrapingError::ReqwestError(e)
-            })
-            .expect("Error getting response from URL, please check the logs.");
+    #[derive(Debug, thiserror::Error, Serialize, Deserialize)]
+    #[allow(clippy::enum_variant_names, dead_code)]
+    pub enum ScrapingError {
+        #[error("Request Error: {0}")]
+        #[serde(skip)]
+        ReqwestError(#[from] reqwest::Error),
 
-        if !response.status().is_success() {
-            error!(
-                "Error: Failed to connect to the website or the website is down. Status is : {:#?}",
-                response.status()
-            );
-            return Err(Box::new(ScrapingError::GlobalError(format!(
-                "Error: Failed to connect to the website or the website is down. Status is : {:#?}",
-                response.status()
-            ))));
-        }
+        #[error("Selector Parsing Error: {0}")]
+        SelectorError(String),
 
-        let body = response
-            .text()
-            .await
-            .map_err(|e| {
-                eprintln!("Failed to get a body from URL: {}", &link_to_paste);
-                ScrapingError::ReqwestError(e)
-            })
-            .unwrap();
+        #[error("Modifying JSON Error: {0}")]
+        #[serde(skip)]
+        FileJSONError(#[from] serde_json::Error),
 
-        let html_document = scraper::Html::parse_document(&body);
-
-        let files_links_selector = scraper::Selector::parse("#plaintext > ul > li > a")
-            .map_err(|err| ScrapingError::SelectorError(err.to_string()))
-            .expect("Error selecting data nodes link");
-        let links: Vec<String> = html_document
-            .select(&files_links_selector) // Select matching elements
-            .filter_map(|element| element.value().attr("href").map(String::from)) // Extract and convert href attributes
-            .collect();
-
-        Ok(links)
+        #[error("Creating File Error in `{fn_name}`: {source}")]
+        #[serde(skip)]
+        CreatingFileError {
+            source: std::io::Error,
+            fn_name: String,
+        },
+        #[error("Global Error: {0}")]
+        #[serde(skip)]
+        GlobalError(String),
     }
 
-    #[tauri::command]
-    pub async fn get_fucking_fast_file_links(
-        url: String,
-    ) -> Result<Vec<String>, Box<ScrapingError>> {
+    async fn get_all_download_links(url: String) -> Result<Vec<String>, Box<ScrapingError>> {
         let response = CUSTOM_DNS_CLIENT
             .get(&url)
             .send()
             .await
             .map_err(|e| {
-                error!("Failed to get response from URL: {}", &url);
-                error!("Error is: {}", e);
+                eprintln!("Failed to get response from URL: {}", &url);
+                eprintln!("Error is: {}", e);
                 ScrapingError::ReqwestError(e)
             })
             .expect("Error getting response from URL, please check the logs.");
 
         if !response.status().is_success() {
-            error!(
+            eprintln!(
                 "Error: Failed to connect to the website or the website is down. Status is : {:#?}",
                 response.status()
             );
@@ -101,28 +77,63 @@ pub mod downloads_function {
             .unwrap();
 
         let html_document = scraper::Html::parse_document(&body);
-
-        let data_nodes_link_selector = scraper::Selector::parse(".entry-content > ul > li > a")
-            .map_err(|err| ScrapingError::SelectorError(err.to_string()))
-            .expect("Error selecting data nodes link");
-
-        let mut nodes_data_links: Vec<String> = Vec::new();
-
-        for element in html_document.select(&data_nodes_link_selector) {
-            // Collect the text content of the element
-            let text_content = element.text().collect::<Vec<_>>().concat();
-            if text_content.contains("FuckingFast") {
+        let a_selector = scraper::Selector::parse("a").unwrap();
+        let mut fucking_fast_links = Vec::new();
+        for element in html_document.select(&a_selector) {
+            if element.text().any(|t| t.contains("FuckingFast")) {
                 if let Some(href) = element.value().attr("href") {
-                    info!("Found href with text 'FuckingFast': {}", &href);
-                    // Process the href using get_links_list
-                    nodes_data_links = get_links_list(href.to_string()).unwrap_or_default();
+                    fucking_fast_links.push(href.to_string());
                 }
             }
         }
 
-        Ok(nodes_data_links)
+        let spoiler_selector =
+            scraper::Selector::parse(".su-spoiler-content.su-u-clearfix.su-u-trim").unwrap();
+        let spoiler_content: Vec<String> = html_document
+            .select(&spoiler_selector)
+            .map(|element| element.inner_html())
+            .collect();
+
+        // Extract all <a> href containing "_fitgirl-repacks.site_" because the others are for updates
+        let mut original_repack_links = Vec::new();
+
+        for content in &spoiler_content {
+            let spoiler_doc = scraper::Html::parse_fragment(content);
+            for element in spoiler_doc.select(&a_selector) {
+                if let Some(href) = element.value().attr("href") {
+                    if href.contains("_fitgirl-repacks.site_") {
+                        original_repack_links.push(href.to_string());
+                    }
+                }
+            }
+        }
+
+        let mut result_links = Vec::new();
+        result_links.extend(original_repack_links);
+
+        Ok(result_links)
     }
 
     #[tauri::command]
-    pub async fn start_data_nodes_downloading() {}
+    pub async fn get_datahoster_links(
+        game_link: String,
+        datahoster_name: String,
+    ) -> Option<Vec<String>> {
+        let all_links = get_all_download_links(game_link).await.unwrap_or_default();
+
+        if !all_links.is_empty() {
+            let filtered_links: Vec<String> = all_links
+                .into_iter()
+                .filter(|link| link.to_lowercase().contains(&datahoster_name))
+                .collect();
+
+            if filtered_links.is_empty() {
+                None
+            } else {
+                Some(filtered_links)
+            }
+        } else {
+            None
+        }
+    }
 }
