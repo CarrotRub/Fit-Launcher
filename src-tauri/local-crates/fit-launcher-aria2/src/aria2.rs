f369f4d49232853b343e5c7d9b9e6edc29725e13
mokurin000 <1348292515a@gmail.com>
2025-06-21 21:59:01 +0800

    feat: support index selecting

--- src-tauri/local-crates/fit-launcher-aria2/src/aria2.rs
+++ src-tauri/local-crates/fit-launcher-aria2/src/aria2.rs
@@ -1,4 +1,5 @@
-use aria2_ws::{Client, TaskOptions};
+use aria2_ws::{Client, Map, TaskOptions};
+use serde_json::Value;
 
 use crate::error::Aria2Error;
 
@@ -28,7 +29,14 @@ pub async fn aria2_add_torrent(
     aria2_client: &Client,
     torrent: Vec<u8>,
     dir: Option<String>,
+    select_file: impl IntoIterator<Item = usize>,
 ) -> Result<String, Aria2Error> {
+    // empty case is safe, aria2 will treat empty select-file as "*".
+    let select_file = select_file
+        .into_iter()
+        .map(|d| d.to_string())
+        .collect::<Vec<String>>()
+        .join(",");
     Ok(aria2_client
         .add_torrent(
             torrent,
@@ -36,6 +44,9 @@ pub async fn aria2_add_torrent(
             Some(TaskOptions {
                 dir,
                 r#continue: Some(true),
+                extra_options: Map::from_iter(
+                    [("select-file".to_string(), Value::String(select_file))].into_iter(),
+                ),
                 ..TaskOptions::default()
             }),
             None,
