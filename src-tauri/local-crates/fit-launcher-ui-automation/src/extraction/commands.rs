d2f0dd976b950838f1cad636709649a480625bb2
莯凛 <mokurin000@gmail.com>
2025-11-08 01:29:42 +0800

    fix: skip non .rar files

--- src-tauri/local-crates/fit-launcher-ui-automation/src/extraction/commands.rs
+++ src-tauri/local-crates/fit-launcher-ui-automation/src/extraction/commands.rs
@@ -16,9 +16,12 @@ pub fn extract_game(dir: PathBuf) -> Result<(), ExtractError> {
         }
 
         let rar_name = rar.file_name();
-        let rar_name_slice = rar_name.to_string_lossy();
+        let rar_name = rar_name.to_string_lossy();
+        if !rar_name.ends_with(".rar") {
+            continue
+        }
 
-        let group_name= rar_name_slice.split_once(".part").map(|(group, _)| group).unwrap_or(&*rar_name_slice);
+        let group_name= rar_name.split_once(".part").map(|(group, _)| group).unwrap_or(&*rar_name);
         groups.entry(group_name.to_owned()).or_insert(rar.path());
     }
     
