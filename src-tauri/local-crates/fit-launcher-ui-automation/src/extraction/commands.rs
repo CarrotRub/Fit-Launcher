63ab69ac230bf6fe72740a813174ff7199b7d298
莯凛 <mokurin000@gmail.com>
2025-11-08 01:24:43 +0800

    fix: RAR extraction for multiple groups

--- src-tauri/local-crates/fit-launcher-ui-automation/src/extraction/commands.rs
+++ src-tauri/local-crates/fit-launcher-ui-automation/src/extraction/commands.rs
@@ -1,17 +1,30 @@
-use std::path::PathBuf;
+use std::{collections::HashMap, path::PathBuf};
 
 use specta::specta;
-use tracing::info;
 
 use crate::{
-    errors::ExtractError, extract_multiple_files, find_first_rar_file,
-    mighty_automation::windows_ui_automation::start_executable_components_args,
+    errors::ExtractError, extract_archive,
 };
 #[tauri::command]
 #[specta]
 pub fn extract_game(dir: PathBuf) -> Result<(), ExtractError> {
-    let rar_file = find_first_rar_file(&dir).ok_or(ExtractError::NoRarFileFound)?;
+    let list = dir.read_dir()?;
+    let mut groups = HashMap::new();
+    for rar in list.flatten() {
+        if rar.metadata()?.is_dir() {
+            continue
+        }
 
-    extract_multiple_files(&rar_file)?;
+        let rar_name = rar.file_name();
+        let rar_name_slice = rar_name.to_string_lossy();
+
+        let group_name= rar_name_slice.split_once(".part").map(|(group, _)| group).unwrap_or(&*rar_name_slice);
+        groups.entry(group_name.to_owned()).or_insert(rar.path());
+    }
+    
+    for rar_file in groups.values() {
+        extract_archive(&rar_file)?;
+    }
+
     Ok(())
 }
