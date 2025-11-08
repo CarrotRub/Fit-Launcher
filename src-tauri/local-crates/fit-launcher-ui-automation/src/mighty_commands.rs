c3a0d8ba00ebfe7fc1ef8daf55c577f7e305bdcd
CarrotRub <95135114+CarrotRub@users.noreply.github.com>
2025-11-08 09:57:46 +0100

    fix: Rename parameter in start_executable function
    Change parameter name from '_path' to 'path' in start_executable function.

--- src-tauri/local-crates/fit-launcher-ui-automation/src/mighty_commands.rs
+++ src-tauri/local-crates/fit-launcher-ui-automation/src/mighty_commands.rs
@@ -10,7 +10,7 @@ use tracing::{error, info};
 /// Do not worry about using String, since the path will always be obtained by dialog through Tauri thus making it always corret for the OS.
 #[tauri::command]
 #[specta]
-pub fn start_executable(_path: String) {
+pub fn start_executable(path: String) {
     // Here, use this **ONLY** for windows OS
     #[cfg(target_os = "windows")]
     match Command::new(&path).spawn() {
