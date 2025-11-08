7ff5d250918f050cb4061896833a0e664640abb8
莯凛 <mokurin000@gmail.com>
2025-11-09 02:33:02 +0800

    enhance: smart file-allocation method

--- src-tauri/local-crates/fit-launcher-aria2/src/aria2.rs
+++ src-tauri/local-crates/fit-launcher-aria2/src/aria2.rs
@@ -10,6 +10,7 @@ pub async fn aria2_add_uri(
     dir: Option<String>,
     filename: Option<String>,
 ) -> Result<String, Aria2Error> {
+    let extra_options = file_allocation_method(dir.as_deref().unwrap_or(".")).await;
     Ok(aria2_client
         .add_uri(
             url,
@@ -18,6 +19,7 @@ pub async fn aria2_add_uri(
                 out: filename,
                 dir,
                 r#continue: Some(true),
+                extra_options,
                 ..TaskOptions::default()
             }),
             None,
@@ -42,7 +44,6 @@ pub async fn aria2_add_torrent(
     let mut options = TaskOptions {
         dir,
         r#continue: Some(true),
-        extra_options: Map::new(),
         ..TaskOptions::default()
     };
 
@@ -59,4 +60,76 @@ pub async fn aria2_add_torrent(
             error!("Failed to add torrent: {}", e);
             Aria2Error::RPCError(format!("Failed to add torrent: {}", e))
         })
+}
+
+async fn file_allocation_method(dir: impl AsRef<str>) -> Map<String, Value> {
+    let dir = dir.as_ref();
+
+    #[cfg(windows)]
+    let file_allocation = {
+        use std::{error::Error, sync::LazyLock};
+
+        use crossbeam_skiplist::SkipMap;
+        use listdisk_rs::{
+            win32::{
+                drive_info::diskindex_by_win32_path,
+                physical_disk::{MediaType, PhysicalDisk},
+            },
+            wmi::WMIConnection,
+        };
+
+        static CACHE: LazyLock<SkipMap<String, MediaType>> = LazyLock::new(|| SkipMap::new());
+
+        let media_type;
+        if let Some(result) = CACHE.get(dir) {
+            media_type = Ok(*result.value());
+        } else {
+            let dir = dir.to_string();
+            media_type = tauri::async_runtime::spawn_blocking(
+                move || -> Result<MediaType, Box<dyn Error + Send + Sync>> {
+                    use std::collections::HashMap;
+
+                    use listdisk_rs::wmi::FilterValue;
+
+                    let wmi_conn =
+                        WMIConnection::with_namespace_path("ROOT\\Microsoft\\Windows\\Storage")?;
+
+                    let disk_index = diskindex_by_win32_path(dir)?.to_string();
+                    let mut filters = HashMap::new();
+                    filters.insert("DeviceId".to_string(), FilterValue::String(disk_index));
+                    Ok(
+                        match wmi_conn.filtered_query::<PhysicalDisk>(&filters)?[..] {
+                            [PhysicalDisk { media_type, .. }, ..] => media_type,
+                            _ => MediaType::Unspecified,
+                        },
+                    )
+                },
+            )
+            .await
+            .unwrap();
+        }
+
+        let result = match &media_type {
+            // for the best performance, we could choose `falloc`,
+            // but the aria2 daemon can be configured as user started,
+            // because we have no idea about it's permission, on SSD/SCM we use none,
+            // to avoid falling back to prealloc, which will cause zero fills.
+            Ok(MediaType::SCM | MediaType::SSD) => "none",
+            Ok(_) => "falloc",
+            Err(e) => {
+                error!("failed to detect MediaType: {e}");
+                "falloc"
+            }
+        };
+
+        CACHE.insert(dir.into(), media_type.unwrap_or(MediaType::HDD));
+        result
+    };
+    #[cfg(not(windows))]
+    // stick to falloc, fallocate() requires no priviledge
+    let file_allocation = "falloc";
+
+    let mut extra_options = Map::new();
+    extra_options.insert("file-allocation".into(), file_allocation.into());
+    extra_options
 }
