e6373f450762e499f11adf20c65e3fb328255510
莯凛 <mokurin000@gmail.com>
2025-11-09 00:52:58 +0800

    enhance: noconfirm flags

--- install_deps.sh
+++ install_deps.sh
@@ -27,8 +27,8 @@ log "Detected $DISTRO-based system"
 # See https://v2.tauri.app/start/prerequisites/#linux
 log "Updating package lists..."
 if [ "$DISTRO" = "debian" ]; then
-    sudo apt update
-    sudo apt install libwebkit2gtk-4.1-dev \
+    sudo apt update -y
+    sudo apt install -y libwebkit2gtk-4.1-dev \
         build-essential \
         aria2 curl wget \
         file \
@@ -37,8 +37,8 @@ if [ "$DISTRO" = "debian" ]; then
         libayatana-appindicator3-dev \
         librsvg2-dev
 elif [ "$DISTRO" = "fedora" ]; then
-    sudo dnf check-update
-    sudo dnf install webkit2gtk4.1-devel \
+    sudo dnf check-update -y
+    sudo dnf install -y webkit2gtk4.1-devel \
             openssl-devel \
             wget aria2 curl \
             file \
@@ -48,7 +48,7 @@ elif [ "$DISTRO" = "fedora" ]; then
     sudo dnf group install "c-development"
 elif [ "$DISTRO" = "arch" ]; then
     # clang.lld is needed as of Rust 1.90
-    sudo pacman -Syu --needed \
+    sudo pacman -Syu --needed --noconfirm \
         aria2 curl lld base-devel \
         webkit2gtk-4.1 \
         wget \
@@ -87,7 +87,7 @@ if command -v cargo >/dev/null 2>&1; then
 else
     log "Installing Rust & Cargo"
     if [ "$DISTRO" = "arch" ]; then
-        sudo pacman -S --needed rustup
+        sudo pacman -S --needed --noconfirm rustup
         rustup default stable
     else
         curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
