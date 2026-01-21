128ff4d50d193ddcfb9a089cb1a1349b64ef38ee
CarrotRub <95135114+CarrotRub@users.noreply.github.com>
2026-01-21 16:33:03 +0100

    fix(dgu): back button not remembering gamehub onload

--- src/pages/Gamehub-01/Gamehub.tsx
+++ src/pages/Gamehub-01/Gamehub.tsx
@@ -3,10 +3,13 @@ import NewlyAddedGames from "./Gamehub-Components-01/Newly-Added-Games-01/Newly-
 import RecentlyUpdatedGames from "./Gamehub-Components-01/Recently-Updated-Games-01/Recently-Updated-Games";
 import FilterBar from "../../components/FilterBar/FilterBar";
 import { GamehubProvider, useGamehub } from "./GamehubContext";
+import { onMount } from "solid-js";
 
 function GamehubContent() {
     const { filters, setFilters, availableGenres, repackSizeRange, originalSizeRange } = useGamehub();
-
+    onMount(() => {
+        localStorage.setItem("latestGlobalHref", "/");
+    })
     return (
         <div class="relative flex flex-col gap-4 divide-y divide-accent/70 w-full h-full overflow-y-auto no-scrollbar">
             <PopularGames />
